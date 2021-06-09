ee861ad7cd941d10ac34a2d510ff74d8a058219a
CCBaxter84 <troyceclark@gmail.com>
2021-06-08 18:33:47 -0600

    Added background color and centered container

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Buefy, {
+  defaultIconPack: 'fas'});
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
